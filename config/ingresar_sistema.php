a820ad57974fc2f5477be91130280e7ad363dd2f
Vurger <miguelnavarrof2002@gmail.com>
2025-05-20 23:57:44 -0400

    aaaa

--- config/ingresar_sistema.php
+++ config/ingresar_sistema.php
@@ -32,6 +32,7 @@ if(isset($_GET["iniciar"])){
                 }elseif ($fila["id_rol"]==2) {
                     
                     echo "ingresaste como trabajador";
+                     header("Location:../inicio.php");
                 }
 
         
