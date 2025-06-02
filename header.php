255b433074a7c5267793d88ce2534eb1da8573b1
Vurger <miguelnavarrof2002@gmail.com>
2025-06-02 12:32:21 -0400

    Header

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 include("./config/ingresar_sistema.php");
 
 
-if ($_SESSION['rol'] == 1) {?>
+if ($_SESSION['rol'] == 2) {?>
 <header>
 <script src="./css/bootstrap-5.3.5-dist/js/bootstrap.bundle.js"></script>
     <div class="header-pequiven">
