89e02c80bf7785c6871d34ec5e7b8c5d4ba4f943
Vurger <miguelnavarrof2002@gmail.com>
2025-06-05 22:25:28 -0400

    Dompdf funcional

--- dompdf.php
+++ dompdf.php
@@ -3,82 +3,174 @@
             use Dompdf\Dompdf;
           ob_start();
 
-<div class="container-fluid">
+          $html.=" <!--DOCTYPE html-->
+<html>
+    <head>
+           <meta charset='utf-8'>
+           <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+        <title>OCS Inventory</title>
+        <link rel='shortcut icon' href='favicon.ico'>
+        <link rel='stylesheet' href='libraries/bootstrap/css/bootstrap.min.css'>
+        <link rel='stylesheet' href='libraries/bootstrap/css/bootstrap-theme.min.css'>
+        <link rel='stylesheet' href='libraries/select2/css/select2.min.css' />
+        <link rel='stylesheet' href='css/dataTables-custom.css'>
+        <link rel='stylesheet' href='libraries/datatable/media/css/dataTables.bootstrap.css'>
+        <link rel='stylesheet' href='css/ocsreports.css'>
+        <link rel='stylesheet' href='css/bootstrap-datetimepicker.css'>
+        <link rel='stylesheet' href='css/header.css'>
+        <link rel='stylesheet' href='css/computer_details.css'>
+        <link rel='stylesheet' href='css/bootstrap-formhelpers.css'>
+        <link rel='stylesheet' href='css/forms.css'>    
+        
+        <style>
+
+        body{
+        font-family: Arial, sans-serif;
+        background-color: #FAFAFA;
+        }
+
+        div {
+            text-align:center;
+
+            
+          }
+
+          h3{
+            color: white;
+            width: auto;
+            text-align: center;
+            position: relative;
+            align-items: center;
+            border: solid 1px #C8142A;
+            border-radius: 5px;
+            background-color: #C8142A;
+        
+        }
+
+        h1{
+            color: #FFFFFF;
+            text-align: center;
+            border: solid 1px #C8142A;
+            border-radius: 5px;
+            background-color: #C8142A;
+        }
+
+        
+        table {
+            width: 100%;
+            max-width: 100%;
+            
+            border-spacing: 0;
+            border-collapse: collapse;
+            border: solid 1px black;
+
+            margin: 0 auto;
+
+            font-size: 10px;
+            page-break-inside: avoid;
+        }
+        
+          td,th {
+             padding: 2px;
+            
+            }
+                thead {
+                    display: table-header-group;
+                    background-color: #E3E3E3;
+                  }
+                  
+                  tr {
+                    page-break-inside: avoid;
+                  }
+
+                  td{
+                text-align: center;  
+                }
+
+                tbody{
+                    background-color: #FFFF;
+
+                }
+              
+        
+        </style>";
+
+$html .= "<div class='container-fluid'>";
           
             include("../Sistema_BRIP/config/conecxion_bd.php");
-                ob_start();
 
             $sql = "SELECT * FROM equipos";
             $result = $conexion->query($sql);
             if ($result->num_rows > 0) {
                
                $html .= "<table id='tabla' class='pequiven-table'>";
-                    "<thead>"
-                    "<tr>"
-                    "<th scope='col'>TAG</th>"
-                    "<th scope='col'>Nombre</th>"
-                    "<th scope='col'>Planta</th>"
-                    "<th scope='col'>Proceso</th>"
-                    "<th scope='col'>Observaciones</th>"
-                    "<th scope='col'>Estado</th>"
-                    "<th scope='col'>Fecha de la ultima revision</th>"
+                $html.=    "<thead>";
+                    $html.="<tr>";
+                    $html.="<th scope='col'>TAG</th>";
+                    $html.="<th scope='col'>Nombre</th>";
+                    $html.="<th scope='col'>Planta</th>";
+                    $html.="<th scope='col'>Proceso</th>";
+                    $html.="<th scope='col'>Observaciones</th>";
+                    $html.="<th scope='col'>Estado</th>";
+                    $html.="<th scope='col'>Fecha de la ultima revision</th>";
                     
-                    "</tr>"
-                "</thead>"
+                    $html.="</tr>";
+                $html.="</thead>";
 
 
 
-                "<tbody class='table-group-divider'>"
+                $html.="<tbody class='table-group-divider'>";
                 
                 while ($row = $result->fetch_assoc()) {
                     
-                    "<tr>"
-                    "<td> echo". $row['id_equipo']  ."</td>"
-                        "<td> echo $row['nombre']</td>"
-                        <td>   $sql1 = "SELECT * FROM plantas WHERE id_planta =". $row['id_planta'];          
+                    $html.="<tr>";
+                    $html.="<td>". $row['id_equipo']  ."</td>";
+                     $html.=   "<td>". $row['nombre'] ."</td>";
+                          $sql1 = "SELECT * FROM plantas WHERE id_planta =". $row['id_planta'];          
                             $resultado = $conexion->query($sql1);
 
                             $row1 = $resultado->fetch_assoc();
                         
-                            echo $row1['nombre_planta']            </td>  
-                        <td>
+                       $html.= "<td>".$row1['nombre_planta'] ."</td> "; 
+                        
                         $sql2 = "SELECT * FROM procesos WHERE id_proceso =". $row['id_proceso'];
                         $resultado2 = $conexion->query($sql2);
 
                         $row2 = $resultado2->fetch_assoc();
-                        echo $row2['nombre_proceso']</td>
-                        <td>echo $row['observacion'] </td>
+                        $html.= "<td>". $row2['nombre_proceso'] ."</td>";
+                        
+                        $html.= "<td>". $row['observacion'] . "</td>";
 
 
                         
 
                         if ($row['estado'] == 'verde' ) {
-                            echo "<td class = 'tabla-verde'> Disponible </td>";  
+                            $html.="<td class = 'tabla-verde'> Disponible </td>";  
                         } else if ($row['estado'] =='amarillo') {
-                            echo "<td class = 'tabla-amarillo'> Baja confiabilidad </td>";
+                            $html.="<td class = 'tabla-amarillo'> Baja confiabilidad </td>";
                         } else {
-                            echo "<td class = 'tabla-rojo'> No disponible </td>";
+                            $html.="<td class = 'tabla-rojo'> No disponible </td>";
                         }
                         
 
 
-                        <td> echo $row['ultima_revision']</td>
+                       $html.= "<td>".$row['ultima_revision']."</td>";
 
 
                        
-                        </tr>
+                       $html.=  "</tr>";
                      
                 }
-                     </tbody>
+                     $html .="</tbody>";
                 
              
             }
             
            
-            </table>
+            $html .="</table>";
        
-                    
-                    $html = ob_get_clean();
 
                     
                     // instantiate and use the dompdf class
@@ -94,9 +186,10 @@
 
                     // Render the HTML as PDF
                    $dompdf->render();
-
+            header("Content-type: application/pdf");
+            header("Content-Disposition: inline; filename= Archivo.pdf");
                     // Output the generated PDF to Browser
-                   $dompdf->stream("archivo.pdf"); 
+                   echo $dompdf->output();
                     
                     $conexion->close();
                     ?>
