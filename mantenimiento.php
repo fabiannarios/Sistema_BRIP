abddceed245aff8a42b5fe89adb456af71232259
Vurger <miguelnavarrof2002@gmail.com>
2025-06-11 21:12:53 -0400

    Wawawa

--- mantenimiento.php
+++ mantenimiento.php
@@ -147,7 +147,7 @@ $resultadoresponsable = $conexion->query($consulta3);
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-1" id="exampleModalLabel">Modal title</h1>
+                    <h1 class="modal-title fs-1" id="exampleModalLabel">AGREGAR RESPONSABLES</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
